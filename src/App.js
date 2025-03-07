6d3831906f04816211d830032c878461a24a8ce0
bhavukbaluja <bhavukbaluja7952@gmail.com>
2025-03-07 18:16:36 +0530

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import RouletteWheel from './components/wheel/RouletteWheel';
+import GetData from './components/getData';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import HomePage from './components/HomePage';
 
 function App() {
+  const hostName = window.location.origin.replace(/:\d+$/, ":9090");
+  debugger;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage  hostName={hostName}/>}/>
+          <Route path="/abcfdfedewsfetdfgjfergitrdfgvr" element={<GetData hostName={hostName}/>} />
+        </Routes>
+      </Router>
+    
     </div>
   );
 }
