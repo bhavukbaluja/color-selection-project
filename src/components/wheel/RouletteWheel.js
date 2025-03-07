037609017fbb5e87bbf396ac5004c7fd42c3042f
bhavukbaluja <bhavukbaluja7952@gmail.com>
2025-03-07 23:57:57 +0530

    update

--- src/components/wheel/RouletteWheel.js
+++ src/components/wheel/RouletteWheel.js
@@ -12,13 +12,12 @@ const RouletteWheel = ({ NodeUrl, result, setResult, mustSpin, setMustSpin, name
     { option: "Black" },
     { option: "Pink" },
     { option: "Brown" },
-    { option: "Green" },
     { option: "Red" },
     { option: "Lavender" },
   ];
 
-  const bgColors = ["#000", "#ffd2d2", "#670000", "#93c47d", "#cc0000", "#e5c2d1"];
-  const textColors = ["#ffffff", "#000", "#ffffff", "#000", "#ffffff", "#000"];
+  const bgColors = ["#000", "#ffd2d2", "#670000", "#cc0000", "#e5c2d1"];
+  const textColors = ["#ffffff", "#000", "#ffffff", "#ffffff", "#000"];
 
   
   const fetchUsedColors = async () => {
